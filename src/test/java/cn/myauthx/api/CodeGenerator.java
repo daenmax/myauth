d4e1d2217b51cd57b922ab10b49402a7fc928de5
JeWang <sdjewang@chinaums.com>
2022-01-06 08:37:14 +0800

    首次提交2

--- src/test/java/cn/myauthx/api/CodeGenerator.java
+++ src/test/java/cn/myauthx/api/CodeGenerator.java
@@ -8,15 +8,18 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 
+/**
+ * 代码生成器
+ */
 public class CodeGenerator {
 
-    private String moudleName = "love";//模块名
+    private String moudleName = "web";//模块名
 
-    private String tableName = "tb_class";//表名
+    private String tableName = "ma_soft_user";//表名
 
     @Test
     public void generate() {
-        FastAutoGenerator.create("jdbc:mysql://localhost:3306/dbtest?serverTimezone=Asia/Shanghai", "dbtest", "dbtest")
+        FastAutoGenerator.create("jdbc:mysql://localhost:3306/myauthtest?serverTimezone=Asia/Shanghai", "myauthtest", "myauthtest")
                 .globalConfig(builder -> {
                     builder.author("DaenMax") // 设置作者
                             //.enableSwagger() // 开启 swagger 模式
@@ -24,17 +27,17 @@ public class CodeGenerator {
                             .outputDir(System.getProperty("user.dir") + "/src/main/java"); // 指定输出目录
                 })
                 .packageConfig(builder -> {
-                    builder.parent("com.example.demo") // 设置父包名
+                    builder.parent("cn.myauthx.api") // 设置父包名
                             .moduleName(moudleName) // 设置父包模块名
                             .pathInfo(Collections.singletonMap(OutputFile.mapperXml, System.getProperty("user.dir") +
                                     "/src/main/resources/mapper")); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
                     builder.addInclude(tableName) // 设置需要生成的表名
-                            .addTablePrefix("tb_") // 设置过滤表前缀
+                            .addTablePrefix("ma_") // 设置过滤表前缀
                             .entityBuilder()
                             .enableLombok()
-                            .superClass("com.example.demo.base.po.baseEntity")
+                            .superClass("cn.myauthx.api.base.po.baseEntity")
                             .idType(IdType.AUTO)
                             .enableChainModel()
                             .controllerBuilder()
